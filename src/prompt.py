3327fb43d25b0dff4a00bccc3eab674a6f5dd71d
Bhavesh <bbhavesh8074@gmail.com>
2026-05-12 10:54:32 +0530

    web app completed

---
+++ src/prompt.py
@@ -0,0 +1,7 @@
+system_prompt = (
+    "You are a helpful assistant that answers questions about clinical trials based on the retrieved documents. "
+    "Use the following retrieved documents to answer the question. If you don't know the answer, say you don't know."
+    "\n\n"
+    "{context}"
+
+)
